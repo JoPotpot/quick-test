7435c19e88cab96dc3244edaf8363f020b3854c4
JPotironRasp <josselin.potiron.d@gmx.fr>
2018-04-02 19:17:11 +0200

    modif script Python pour exercice 2

--- test-python/bin/exercice_python.py
+++ test-python/bin/exercice_python.py
@@ -2,14 +2,15 @@
 #-*- coding: utf-8 -*-
 
 import requests
+import json
 
 f = open('../output/liste_100_posts.json','w')
 f.write('Liste_des_100_premiers_posts_depuis_https://jsonplaceholder.typicode.com/posts')
-f.close()
 
 for i in range (1,101):
 	r = requests.get('https://jsonplaceholder.typicode.com/posts/'+str(i))
-	f = open('../output/liste_100_posts.json','a') 
+
 	f.write('\n')
 	f.write(str(r.json())) 
-	f.close()
+
+f.close()
