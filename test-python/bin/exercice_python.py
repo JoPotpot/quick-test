e5a6f8b20b69e32764f5eff83abe0ed2f67ee6a7
JPotironRasp <josselin.potiron.d@gmx.fr>
2018-04-06 18:01:54 +0200

    Reecriture en PEP8 du script Python

--- test-python/bin/exercice_python.py
+++ test-python/bin/exercice_python.py
@@ -1,30 +1,28 @@
 #!/usr/bin/python2.7
-#-*- coding: utf-8 -*-
-# à lancer avec l'argument nb_articles
-# exemple : "python exercice_python.py 100" si on veut récupérer 100 articles 
-# fichier json généré à récupérer sous quick-test/test-python/output/
+# -*- coding: utf-8 -*-
+# A lancer avec l'argument nb_articles
+# Exemple : "python exercice_python.py 100" si on veut récupérer 100 articles
+# Fichier json généré à récupérer sous quick-test/test-python/output/
 
-#Liste des 100 premiers posts depuis https://jsonplaceholder.typicode.com/posts'
+# Liste de 100 premiers posts depuis https://jsonplaceholder.typicode.com/posts
 
 import requests
 import json
 import sys
 
 nb_articles = int(sys.argv[1])
-file = open('../output/liste_100_posts.json','w')
+file = open('../output/liste_100_posts.json', 'w')
 
-
-jsonText = requests.get('https://jsonplaceholder.typicode.com/posts/1').json()
+json_text = requests.get('https://jsonplaceholder.typicode.com/posts/1').json()
 file.write('[\n')
-file.write(json.dumps(jsonText))
+file.write(json.dumps(json_text))
 
-for i in range (2,nb_articles+1):
+for i in range(2, nb_articles+1):
 
-	jsonText = requests.get('https://jsonplaceholder.typicode.com/posts/'+str(i)).json()
-	file.write(',\n')
-	file.write(json.dumps(jsonText))
+    json_text = requests.get('https://jsonplaceholder.'
+                             'typicode.com/posts/' + str(i)).json()
+    file.write(',\n')
+    file.write(json.dumps(json_text))
 
-	
-	
 file.write('\n]')
 file.close()
